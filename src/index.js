6adaaf65c237f3cedc3b42e5c86ab8673b263644
Suman-prog <vangalasuman1995@gmail.com>
2020-06-11 16:29:34 +0530

    Task-Tracker-Changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from '../src/Store/reducer';
 
+const store=createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const app=(
+  <Provider store={store}>
+    <React.StrictMode>
+    <App/>
+    </React.StrictMode>
+    
+  </Provider>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
